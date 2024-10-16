677674a50192264cb1656cd55f0667f0a2030f8d
KolbotBou <kolbot.bou@gmail.com>
2024-10-16 22:53:59 +0700

    Rendering Some Library Values & Its UI Design using BStrap5

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
 
+# Import all the Models we need to Access Data and Information to be Shown on Page Below
+from .models import Book, BookCopy, Genre, Author, Language
+
 # Create your views here.
 
 # When MAIN URL /library is accessed, index.html file will be viewed / shown
 def index(request):
-    return render(request, 'index.html')
+
+    # To Count All The Details / Data in The Library Database for Shown
+    num_books = Book.objects.all().count()
+    
+    num_book_copies = BookCopy.objects.all().count()
+
+    num_genres = Genre.objects.all().count()
+
+    num_authors = Author.objects.all().count()
+
+    num_languages = Language.objects.all().count()
+
+        # Counting Total Number of Book Copy with Status 'a' : 'available
+    num_book_available = BookCopy.objects.filter(status__exact='a').count()
+
+    # Creating a PAYLOAD Dictionary containing Key-Value for Database Info we want to show
+    payloads = {
+        'num_books' : num_books,
+        'num_book_copies' : num_book_copies,
+        'num_genres' : num_genres,
+        'num_authors' : num_authors,
+        'num_languages' : num_languages,
+        'num_book_available' : num_book_available
+    }
+
+
+    return render(request, 'index.html', context=payloads)
+###
+'''Context is a Key-Value Objects that we use to HOLD VALUES we RENDER from MODELS'''
+###
 
 # When about_us URL is accessed, about_us.html file will be viewed / shown
 def about_us(request):
